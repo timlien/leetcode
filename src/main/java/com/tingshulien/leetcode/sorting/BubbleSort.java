b5ff7d10faa87a45026e216c601230d48405f531
Tim Lien <tim@1001f.com>
2022-09-16 17:01:41 +0800

    Add bubble sort algorithm Add insertion sort algorithm

---
+++ src/main/java/com/tingshulien/leetcode/sorting/BubbleSort.java
@@ -0,0 +1,21 @@
+package com.tingshulien.leetcode.sorting;
+
+public class BubbleSort {
+
+    public static void sort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
---
+++ src/main/java/com/tingshulien/leetcode/sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+package com.tingshulien.leetcode.sorting;
+
+public class InsertionSort {
+
+    public static void sort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            swap(nums, i, minIndex);
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
