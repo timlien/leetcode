0711897d59ec6c49c4aed404117d4b8411892002
Tim Lien <timlientw@gmail.com>
2022-11-11 16:16:04 +0800

    Finish 104 Maximum Depth of Binary Tree

---
+++ src/main/java/com/tingshulien/leetcode/binarytree/MaximumDepthOfBinaryTree_104.java
@@ -0,0 +1,22 @@
+package com.tingshulien.leetcode.binarytree;
+
+import com.tingshulien.leetcode.utility.TreeNode;
+
+/**
+ * <a href="https://leetcode.com/problems/maximum-depth-of-binary-tree/description/">104. Maximum Depth of Binary Tree</a>
+ * <p>
+ * Given the root of a binary tree, return its maximum depth.
+ * <p>
+ * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ */
+public class MaximumDepthOfBinaryTree_104 {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+}
---
+++ src/main/java/com/tingshulien/leetcode/utility/TreeNode.java
@@ -0,0 +1,15 @@
+package com.tingshulien.leetcode.utility;
+
+public class TreeNode {
+
+    public int val;
+
+    public TreeNode left;
+
+    public TreeNode right;
+
+    public TreeNode (int val) {
+        this.val = val;
+    }
+
+}
