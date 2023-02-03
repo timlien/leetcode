1d7d9bd5d675e7f6ad378c25d71156d1a9bd0410
Tim Lien <timlientw@gmail.com>
2023-02-04 01:43:32 +0800

    Finish 35 Search Insert Position

---
+++ src/main/java/com/tingshulien/leetcode/binarysearch/SearchInsertPosition_35.java
@@ -0,0 +1,34 @@
+package com.tingshulien.leetcode.binarysearch;
+
+/**
+ * <a href="https://leetcode.com/problems/search-insert-position/description/">35. Search Insert Position</a>
+ * <p>
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+ * <p>
+ * You must write an algorithm with O(log n) runtime complexity.
+ */
+public class SearchInsertPosition_35 {
+
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (target == nums[mid]) {
+                return mid;
+            }
+
+            if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+}
