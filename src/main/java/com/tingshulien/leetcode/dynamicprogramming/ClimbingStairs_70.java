dc79c97ae12fd601b280b5c0a65061446b9c72cc
Tim Lien <timlientw@gmail.com>
2023-05-08 16:50:28 +0800

    Finish 70 Climbing Stairs

---
+++ src/main/java/com/tingshulien/leetcode/dynamicprogramming/ClimbingStairs_70.java
@@ -0,0 +1,33 @@
+package com.tingshulien.leetcode.dynamicprogramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <a href="https://leetcode.com/problems/climbing-stairs/description/">70. Climbing Stairs</a>
+ * <p>
+ * You are climbing a staircase. It takes n steps to reach the top.
+ * <p>
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ */
+public class ClimbingStairs_70 {
+
+    private Map<Integer, Integer> countBySteps = new HashMap<>();
+
+    public int climbStairs(int n) {
+        return dp(0, n);
+    }
+
+    private int dp(int i, int n) {
+        if (i >= n - 1) {
+            return 1;
+        }
+
+        if (!countBySteps.containsKey(i)) {
+            countBySteps.put(i, dp(i + 1, n) + dp(i + 2, n));
+        }
+
+        return countBySteps.get(i);
+    }
+
+}
